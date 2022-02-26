d09100b9cb1560e0e37bcd6c70268934a321fc95
bhivam <shivamkajaria@gmail.com>
2022-02-26 17:02:07 -0500

    Detects a change in URL but cannot grab the button as of right now

---
+++ background.js
@@ -0,0 +1,31 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.notif === "showNotification") {
+        // show notification
+        console.log("4")
+
+    }
+})
+
+let currentURL = ""
+
+chrome.tabs.onActivated.addListener(activeInfo => {
+    let tabId = activeInfo.tabId
+    chrome.tabs.get(tabId, (tab) => {
+    currentURL = tab.url
+    console.log("3: " + currentURL)
+    })
+    })
+
+chrome.tabs.onUpdated.addListener((tabID, changeInfo, tab) => {
+    console.log("1")
+    if (currentURL !== changeInfo.url) {
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, {changedURL: "changed"}, async (response) => {
+                console.log("5")
+                console.log(response)
+            })
+        })
+        currentURL = changeInfo.url
+        console.log("2: " + currentURL)
+    }
+})
--- purchase_detection.js
+++ purchase_detection.js
@@ -1,2 +1,19 @@
-let button = document.getElementsByClassName("Blockreact__Block-sc-1xf18x6-0 Buttonreact__StyledButton-sc-glfma3-0 dpXlkZ fzwDgL")[0]
-button.addEventListener("click", () => console.log("hello"))
+
+let button = ""
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("grabbed button")
+    sendResponse({gotButton: "gotButton"})
+    if (request.changedURL === "changed") {
+        console.log("1")
+        button = document.getElementsByClassName("Blockreact__Block-sc-1xf18x6-0 Buttonreact__StyledButton-sc-glfma3-0 dpXlkZ fzwDgL")[0]
+        console.log(button)
+    }   
+})
+
+button.addEventListener("click", () => {
+    console.log("hello")
+    chrome.runtime.sendMessage({notif: "showNotification"}, (response) => {
+        console.log(response)
+    })
+    })
