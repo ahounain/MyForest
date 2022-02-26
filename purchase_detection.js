51f50b299f811ffd271f28105e36a22d59762e5e
bhivam <shivamkajaria@gmail.com>
2022-02-26 02:11:51 -0500

    can detect user clicking buy now button

--- purchase_detection.js
+++ purchase_detection.js
@@ -1,1 +1,2 @@
-console.log("hello")
+let button = document.getElementsByClassName("Blockreact__Block-sc-1xf18x6-0 Buttonreact__StyledButton-sc-glfma3-0 dpXlkZ fzwDgL")[0]
+button.addEventListener("click", () => console.log("hello"))
